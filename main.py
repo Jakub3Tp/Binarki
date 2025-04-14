cd0e14c98bc9db8f56a87da5d1833316621cc1d8
student06 <jakub.gola2@teb.edu.pl>
2025-04-14 14:15:19 +0200

    14.04.2025

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+#Zamiana na Binarne
+#Wczytaj liczby dziecietne.txt i zapisz je na liczby binarne w pliku binarne.txt
 
+with open("dziesietne.txt", 'r') as file:
+    lines = file.read()
+    lines = lines.split("\n")
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+binarNumber = []
+for number in lines:
+    result = ""
+    number_int = int(number)
+    while number > 0:
+        result += str(number_int % 2)
+        number_int //= 2
+    result = result[::-1]
+    binarNumber.append(result)
+
+with open("binarne.txt", 'w') as file:
+    file.write("\n".join(number))
