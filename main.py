65426b79b361fbd46cef306fd651641ed0d8d731
student06 <jakub.gola2@teb.edu.pl>
2025-04-14 14:59:36 +0200

    14.04.2025

--- main.py
+++ main.py
@@ -15,10 +15,8 @@ for number in lines:
         number_int //= 2
     result = result[::-1]
     binarNumber.append(result)
-"""
 
-#wersja alternatywna
-"""
+ALternatywna metoda:
     for number in file:
         binarNumber.append(bin(number))
 """
@@ -28,12 +26,17 @@ with open("binarne2.txt", 'r') as file:
     lines = file.read()
     lines = lines.split('\n')
 
+#alter methoda
+for number in lines:
+    print(int(number, base=2))
+
+'''
 for number in lines:
     result = int(number[0])
     for n in range(1, len(number)):
         result = result * 2 + int(number[n])
     print(result)
-
+'''
 
 #with open("binarne.txt", 'w') as file:
 #    file.write("\n".join(binarNumber))
